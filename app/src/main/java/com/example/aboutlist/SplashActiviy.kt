124e90d98d4c59a88b2a8f6ba983299a6ca6b1a6
jimin0127 <ckdgh4662@naver.com>
2020-09-13 01:02:04 +0900

    splash.xml, SplashActiviy.kt 파일 작성, splash 구현, 위에 뜨는 바 제거, 배경 흰색으로 설정 로그인화면

--- app/src/main/java/com/example/aboutlist/SplashActiviy.kt
+++ app/src/main/java/com/example/aboutlist/SplashActiviy.kt
@@ -3,12 +3,15 @@ package com.example.aboutlist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.os.SystemClock
 
-class MainActivity : AppCompatActivity() {
+class SplashActiviy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_login)
+
+        SystemClock.sleep(1200)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }

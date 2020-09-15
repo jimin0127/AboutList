6d3b9a349ebb7766ee970a79cc7d53e32fac08f2
jimin0127 <ckdgh4662@naver.com>
2020-09-16 01:07:58 +0900

    firebase 환경설정(빌드 그래들 추가), firebase에 데이터 쓰기

--- app/src/main/java/com/example/aboutlist/MainActivity.kt
+++ app/src/main/java/com/example/aboutlist/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.aboutlist
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         startActivity(Intent(this, LoginActivity::class.java))
+
     }
 }

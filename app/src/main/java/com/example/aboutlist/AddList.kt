539ace2a31aa2e3d7c6377560ad51f4ca1369cef
03one02one <ehrkd1021@naver.com>
2020-09-16 01:11:47 +0900

    bar_btn.PNG, newlist_btn.PNG, round_edittext.xml,수정

---
+++ app/src/main/java/com/example/aboutlist/AddList.kt
@@ -0,0 +1,13 @@
+package com.example.aboutlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RatingBar
+import androidx.appcompat.app.AlertDialog
+
+class AddList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addlist)
+    }
+}
--- app/src/main/java/com/example/aboutlist/LoginActivity.kt
+++ app/src/main/java/com/example/aboutlist/LoginActivity.kt
@@ -11,7 +11,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         var goSingup_btn : Button = findViewById(R.id.goSignup_btn)
         goSingup_btn.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))

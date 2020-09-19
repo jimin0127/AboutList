fdf4f856968e2eea185d5f3b226bd6fee202e17f
jimin0127 <ckdgh4662@naver.com>
2020-09-19 09:55:05 +0900

    Firebase Authentication 이메일, 비밀번호 이용 사용자 추가 구현

--- app/src/main/java/com/example/aboutlist/SignupActivity.kt
+++ app/src/main/java/com/example/aboutlist/SignupActivity.kt
@@ -3,15 +3,22 @@ package com.example.aboutlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
-import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_signup.*;
+
+import android.widget.Toast
+import com.google.firebase.auth.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 
 class SignupActivity : AppCompatActivity() {
+
+
+
+
+    private var firebaseAuth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
 
         back_btn.setOnClickListener{
             finish()
@@ -19,32 +26,24 @@ class SignupActivity : AppCompatActivity() {
 
         //회원가입 페이지에서 다음 버튼 누르기
         signup_btn.setOnClickListener {
-            var signupNick_input = signupNick_input.text.toString() 
-            var signupId_input = signupId_input.text.toString()
-            var signupPassword_input = signupPassword_input.text.toString()
+            firebaseAuth = FirebaseAuth.getInstance()
 
-            //데이터 베이스 연결(firebase)
-            val database = FirebaseDatabase.getInstance()
-            
-            //데이터베이스 테이블 연결(테이블 명 : User)
-            val UserDataBase = database.getReference("User")
-
-            //User 증가 구현해야함
-            //User 테이블 밑에 User2라는 유저 안에 닉네임과 아이디, 패스워드가 들어감
-            UserDataBase.child("User2").child("nickname").setValue(signupNick_input)
-            UserDataBase.child("User2").child("id").setValue(signupId_input)
-            UserDataBase.child("User2").child("password").setValue(signupPassword_input)
-
-            //다이얼로그 띄우기
-            var dialog = AlertDialog.Builder(this)
-            //확인 버튼 누르면 회원가입 페이지 종료
-            dialog.setPositiveButton("확인") { dialog, id ->
-                finish()
-            }
-            dialog.setTitle("회원가입")
-            dialog.setMessage("회원가입이 완료되었습니다.")
-            //다이얼로그 보여주기
-            dialog.show()
+            var signupNick_input = signupNick_input.text.toString().trim()
+            //var signupId_input = signupId_input.text.toString()
+            var signupPassword_input = signupPassword_input.text.toString().trim()
+            createEamil(signupNick_input, signupPassword_input)
         }
+    }
+
+    private fun createEamil(email : String, password : String) {
+        firebaseAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful) {
+                    val user = firebaseAuth?.currentUser
+                    Toast.makeText(this, "Authentication success", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }

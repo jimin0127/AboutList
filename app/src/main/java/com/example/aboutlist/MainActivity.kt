e5965cb3deb1d832c5d790cf10df80c4031edf7b
03one02one <ehrkd1021@naver.com>
2020-10-02 23:39:36 +0900

    main 완성

--- app/src/main/java/com/example/aboutlist/MainActivity.kt
+++ app/src/main/java/com/example/aboutlist/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.aboutlist
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var add : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         startActivity(Intent(this, LoginActivity::class.java))
+
+        add = findViewById(R.id.add_list)
+
+        add.setOnClickListener{
+            val intent = Intent(this, AddList::class.java)
+        }
 
     }
 }
